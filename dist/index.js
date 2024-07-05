4758568bbaa6b47763a83a15195e78bb5b5877a3
ahmet-dede <ahmetdede6108@gmail.com>
2024-07-05 19:54:59 +0300

    header-main-footer completed

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,13 @@ function toggleClass() {
   navbar.classList.toggle("active");
   document.querySelector("main").style.display = isOpen ? "block" : "none";
 }
+
+for (let index = 0; index < 6; index++) {
+  function togglePanel(index) {
+    const panel = document.querySelectorAll(".div-toggle");
+    const button = document.querySelectorAll(".questions button");
+    const isOpen = button[index].classList.contains("toggled");
+    button[index].classList.toggle("toggled");
+    panel[index].style.display = isOpen ? "none" : "flex";
+  }
+}
